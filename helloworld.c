51ae8121349c101224bdd435571dcfe273f50a43
Elene Menabde <elenemenabde@Elenes-MacBook-Air.local>
2026-08-10 11:19:41 +0300

    Add binary number explanation

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+int main()
+{
+    int decimal = 0;
 
-int main() {
-    printf("Hello, World!\n");
+    printf("Binary number: 00101101\n\n");
+
+    printf("128: 0\n");
+    printf("64:  0\n");
+    printf("32:  1\n");
+    printf("16:  0\n");
+    printf("8:   1\n");
+    printf("4:   1\n");
+    printf("2:   0\n");
+    printf("1:   1\n\n");
+
+    decimal = 32 + 8 + 4 + 1;
+
+    printf("Calculation: 32 + 8 + 4 + 1\n");
+    printf("Decimal: %d\n", decimal);
+
     return 0;
 }
